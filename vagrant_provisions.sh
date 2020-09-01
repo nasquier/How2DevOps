808bb029f627ea7df6730c2a3c117c8bab78c8bd
nasquier <nicolas.asquier@gmail.com>
2020-09-01 20:33:38 +0200

    Correction d'une coquille dans le run

--- vagrant_provisions.sh
+++ vagrant_provisions.sh
@@ -1,3 +1,6 @@
+# Change DNS server to Google's
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+
 # This script will execute in the VM after its creation
 # apache2 installation 
 apt update
@@ -13,10 +16,9 @@ a2ensite lostgems.conf # vhost activation
 systemctl reload apache2
 
 # Downloading the sources for the new site
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/nullgunzip
 wget https://www.github.com/nasquier/lostgems/archive/master.tar.gz
-tar -xf master.tar
-rm master.tar
+tar -xzf master.tar.gz
+rm master.tar.gz
 mkdir /var/www/local/lostgems -p
 mv lostgems-master/* /var/www/local/lostgems
 rm lostgems-master -r
